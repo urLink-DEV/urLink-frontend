0e878d58c23ec38ffc2e30fdaab6605aa8be7947
leesoo <isoo7510@gmail.com>
2020-04-04 13:25:50 +0900

    config arc http

--- src/commons/http.js
+++ src/commons/http.js
@@ -1,3 +1,12 @@
-export const http = () => {
-    
-}
+import axios from 'axios';
+
+const http = axios.create({
+    baseURL: 'http://localhost:3000',
+});
+
+export default {
+    'GET': http.get(),
+    'POST': http.post(),
+    'PUT': http.put(),
+    'DELETE': http.delete(),
+};
