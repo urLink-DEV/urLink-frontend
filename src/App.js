77df66baa44fd0abc428478aad64f152f9703799
leesoo7595 <isoo7510@gmail.com>
2020-04-04 10:02:13 +0900

    feature oauth & arc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Login from './containers/Login';
+import GoogleLogin from './containers/GoogleLogin';
 import './App.scss';
 
 function App() {
   return (
     <div className={'app'}>
       hello URLink
+      <Login />
+      <GoogleLogin />
     </div>
   );
 }
