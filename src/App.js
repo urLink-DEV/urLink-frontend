4a4ca7601e7cc02affa7ba81991293b5dba32379
leesoo <isoo7510@gmail.com>
2020-04-09 01:04:12 +0900

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import {Switch, BrowserRouter, Route} from "react-router-dom";
 import Login from './containers/Login';
-import GoogleLogin from './containers/GoogleLogin';
+// import GoogleLogin from './containers/GoogleLogin';
 import './App.scss';
+
+function Main() {
+  return (
+    <div>
+      hello URLink
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className={'app'}>
-      hello URLink
-      <Login />
-      {/* <GoogleLogin /> */}
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path={"/login"} component={Login}/>
+        <Route exact path={"/"} component={Main} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
