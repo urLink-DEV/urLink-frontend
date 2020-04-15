3792e610a09a7fcb04b70163713e605be6252156
yjkwon07 <dudwns9073@nate.com>
2020-04-15 12:09:05 +0900

    - axios setting - accesstoken check - main, user Login test component

--- src/commons/http.js
+++ src/commons/http.js
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import Axios from 'axios';
+import auth from './auth';
 
 /**
  * 	* Basic
@@ -10,88 +11,80 @@ import axios from 'axios';
  * * Header parameter name: Authorization
 */
 
-const server = {
-    scheme: "http",
-    host: "bc4407b5.ngrok.io/api/v1",
-    port: "",
-    addr: function () {
-		return (this.scheme ? this.scheme + ":" : "") + "//" + this.host + (this.port ? ":" + this.port : "");
-    },
-    api: {
-        GET_TOKEN: "user/token/", // * 토근 생성
-        UPDATE_TOKEN: "user/token/refresh/", // * 토큰 갱신
-        CHECK_TOKEN: "user/token/verify/", // * 토큰 검사
+const api = {
+	GET_TOKEN: "user/token/", // * 토근 생성
+	UPDATE_TOKEN: "user/token/refresh/", // * 토큰 갱신
+	CHECK_TOKEN: "user/token/verify/", // * 토큰 검사
 
-        G_MEMBER_REGISTER: "user/google/sign-up/", // * 구글 회원가입
-        G_MEMBER_LOGIN: "user/google/sign-in/", // * 구글 로그인
+	G_MEMBER_REGISTER: "user/google/sign-up/", // * 구글 회원가입
+	G_MEMBER_LOGIN: "user/google/sign-in/", // * 구글 로그인
 
-        N_MEMBER_REGISTER: "user/sign-up/", // * 일반 회원가입
-        N_MEMBER_LOGIN: "user/sign-in/", // * 일반 로그인
-        
-        MEMBER_LOGOUT: "user/sign-out/", // * 로그아웃
-
-        MEMBER: "user/", // * 회원정보 관련
-	},
+	N_MEMBER_REGISTER: "user/sign-up/", // * 일반 회원가입
+	N_MEMBER_LOGIN: "user/sign-in/", // * 일반 로그인
 	
-	getAxios: function (apiName, query, cb) {
-        let url = this.addr() + '/' + apiName;
-        let query_options = query || {};
+	MEMBER_LOGOUT: "user/sign-out/", // * 로그아웃
 
-		axios.get(url, query_options)
-			.then(function ({ data }) {
-				cb({ data, err: '' });
-			}).catch((e) => {
-				cb({ data: {}, err: e });
-			});
-	},
-	
-    postAxios: function (apiName, query, cb) {
-        let url = this.addr() + '/' + apiName;
-		let query_options = query || {};
-		
-        axios.post(url, query_options)
-			.then(function ({ data }) {
-				cb({ data, err: '' });
-			}).catch((e) => {
-				cb({ data: {}, err: e });
-			});
-    },
-
-	putAxios: function (apiName, query, cb) {
-        let url = this.addr() + '/' + apiName;
-		let query_options = query || {};
-
-		axios.put(url, query_options)
-			.then(function ({ data }) {
-				cb({ data, err: '' })
-			}).catch((e) => {
-				cb({ data: {}, err: e })
-			});
-    },
-	
-	patchAxios: function (apiName, query, cb) {
-        let url = this.addr() + '/' + apiName;
-		let query_options = query || {};
-		
-		axios.patch(url, query_options)
-			.then(function ({ data }) {
-				cb({ data, err: '' })
-			}).catch((e) => {
-				cb({ data: {}, err: e })
-			});
-	},
-	
-	deleteAxios: function (apiName, query, cb) {
-        let url = this.addr() + '/' + apiName;
-		let query_options = query || {};
-
-		axios.delete(url, query_options)
-			.then(function ({ data }) {
-				cb({ data, err: '' })
-			}).catch((e) => {
-				cb({ data: {}, err: e })
-			});
-	},
+	MEMBER: "user/", // * 회원정보 관련
 };
 
-export default server;
+const axiosSetting = {
+    scheme: "http",
+    host: "ef5fdc13.ngrok.io/api/v1",
+    port: "",
+    server: function() {
+		return (this.scheme ? this.scheme + ":" : "") + "//" + this.host + (this.port ? ":" + this.port : "");
+	},
+	redirectPage: function(){
+		window.location.href = "/login";
+	}
+};
+
+const axios = Axios.create({
+	baseURL: axiosSetting.server(),
+	timeout: 2000,
+});
+
+if(localStorage.getItem('accessToken')) axios.defaults.headers.common['Authorization'] = "JWT " + localStorage.getItem('accessToken');
+
+// * Add a request interceptor
+axios.interceptors.request.use(function (config) {
+	// * Do something before request is sent
+	if(localStorage.getItem('accessToken')) config.headers.Authorization = "JWT " + localStorage.getItem('accessToken');
+	return config;
+}, function (error) {
+	return Promise.reject(error)
+});
+
+// * Add a response interceptor
+axios.interceptors.response.use((response) => {
+	return response;
+}, async (error) => {
+		const status = error.response.status
+		const response = error.response.data
+		const originalRequest = error.config;
+		const url = originalRequest.url;
+
+		// * accessToken 만료
+		if (status === 401 && url !== api.UPDATE_TOKEN && response.code === 'token_not_valid') {
+			const refreshToken = localStorage.getItem("refreshToken");
+			try {
+				const response = await auth.refreshAccessToken(refreshToken);
+				if(!response) {
+					this.redirectPage(); // * "token_not_valid login!!
+				}
+				else if(response && localStorage.getItem('accessToken')) {
+					axios.defaults.headers.common['Authorization'] = "JWT " + localStorage.getItem('accessToken');
+					return axios.request(originalRequest);
+				}
+			} catch (error) {
+				this.redirectPage(); // * token_not_valid login!!
+			}
+		}
+		// * login 필수
+		else if(status=== 401 && response.detail.indexOf("authentication credentials") >- 1){
+			this.redirectPage(); // * no authentication login!!
+		}
+		return Promise.reject(error);
+});
+
+export { axios , api };
---
+++ src/test/MainTest.js
@@ -0,0 +1,25 @@
+import React , {useState} from "react";
+import auth from '../commons/auth';
+import LoginTest from './LoginTest';
+
+function MainTest() {
+    const [login, setLogin] = useState(false);
+
+    if(login){
+        return <LoginTest/>;
+    }
+
+    (async () => {
+        const check = await auth.tokenCheck();
+        if(!check) setLogin(true);
+    })();
+
+    return (
+        <div>
+            <h1>Main Test JWT 토큰 반응 확인</h1>
+            로그인 상태면 볼 수 있어요.
+        </div>
+    );
+}
+
+export default MainTest;
