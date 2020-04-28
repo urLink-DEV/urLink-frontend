271572a20dafb6ddc1e6db0c73a5242803501bfa
lstsun <lstsun@fin2b.com>
2020-04-28 20:54:21 +0900

    :bug: feature google oauth login in react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+/* global chrome */
 import React from 'react';
 import {Link, Router} from "react-chrome-extension-router";
 import Login from './containers/Login';
 
 function Main() {
+  console.log('chrome', chrome);
+
+  
   return (
     <div>
       hello Urlink
       <Link component={Login}>
-        Login
+        login
       </Link>
     </div>
   )
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,3 +1,4 @@
+/* global chrome */
 import React from 'react';
 
 export default function Login() {
@@ -7,12 +8,34 @@ export default function Login() {
         console.log('submit');
     }
 
+    const onClickLogin = e => {
+        console.log('event', e);
+        chrome.identity.getAuthToken({interactive: true}, function(token) {
+          let init = {
+            method: 'GET',
+            async: true,
+            headers: {
+              Authorization: 'Bearer ' + token,
+              'Content-Type': 'application/json'
+            },
+            'contentType': 'json'
+          };
+          fetch(
+              'https://people.googleapis.com/v1/contactGroups/all?key=AIzaSyD9VzCBawbBZR6LNJdbFWVJF_HYdiGQc_Y',
+              init)
+              .then((response) => response.json())
+              .then(function(data) {
+                console.log(data)
+              });
+        });
+      }
+
     return (
         <div>
             <button onSubmit={onSubmit}>
                 normal login
             </button>
-            <button className='oauth-button'>
+            <button onClick={onClickLogin}>
                 google login
             </button>
         </div>
