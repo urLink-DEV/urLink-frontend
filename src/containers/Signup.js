8c502c4acaa3714c87cc930ccc2fc0aa03ab87a5
leesoo7595 <isoo7510@gmail.com>
2020-05-02 16:46:14 +0900

    :sparkles: feature google sign up

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -4,6 +4,8 @@ export const onClickSignup = e => {
   console.log('submit');
 }
 
+let retry = true;
+
 export const onClickGoogleSignup = e => {
   chrome.identity.getAuthToken({interactive: true}, function(token) {
     let init = {
@@ -15,12 +17,74 @@ export const onClickGoogleSignup = e => {
       },
       'contentType': 'json'
     };
-    fetch(
-      'https://people.googleapis.com/v1/contactGroups/all?key=AIzaSyD9VzCBawbBZR6LNJdbFWVJF_HYdiGQc_Y',
-      init)
+    requestStart(init)
       .then((response) => response.json())
-      .then(function(data) {
+      .then((data) => {
         console.log(data)
-      });
-  });
+        console.log('token', token)
+        return requestGoogleSignUp(token)
+      })
+      .then(res => {
+        if (res.status >= 401 && retry) {
+          retry = false
+          chrome.identity.removeCachedAuthToken({token}, onClickGoogleSignup)
+        }
+      })
+      .catch(e => console.log('error', e));
+  })
 }
+
+
+function requestStart(init) {
+  return fetch(
+    'https://people.googleapis.com/v1/contactGroups/all?key=AIzaSyD9VzCBawbBZR6LNJdbFWVJF_HYdiGQc_Y',
+    init)
+}
+
+function requestGoogleSignUp(token) {
+  const headers = new Headers();
+  headers.append('Content-Type', 'application/json')
+  return fetch('http://15.165.198.243/api/v1/user/google/sign-up/', {
+    method: 'POST',
+    headers,
+    body: {token},
+  })
+}
+
+// function xhrWithAuth(method, url, interactive, callback) {
+//   var access_token;
+
+//   var retry = true;
+
+//   getToken();
+
+//   function getToken() {
+//     chrome.identity.getAuthToken({ interactive: interactive }, function(token) {
+//       if (chrome.runtime.lastError) {
+//         callback(chrome.runtime.lastError);
+//         return;
+//       }
+
+//       access_token = token;
+//       requestStart();
+//     });
+//   }
+
+//   function requestStart() {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open(method, url);
+//     xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+//     xhr.onload = requestComplete;
+//     xhr.send();
+//   }
+
+//   function requestComplete() {
+//     if (this.status == 401 && retry) {
+//       retry = false;
+//       chrome.identity.removeCachedAuthToken({ token: access_token },
+//                                             getToken);
+//     } else {
+//       callback(null, this.status, this.response);
+//     }
+//   }
+// }
