5f19c19004b9c75d5bc5b8e5d8df4340fccb8604
yjkwon07 <dudwns9073@nate.com>
2020-05-16 01:55:15 +0900

    category testPage Complete - categoryAPI func. [C] - categoryTest Page [C]

--- src/test/CategoryTest.js
+++ src/test/CategoryTest.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import {getCategory,writeCategory,updateCategory,deleteCategory} from '../commons/category';
+import React, { useState, useEffect } from 'react';
+import CategoryAPI from '../commons/CategoryAPI';
 
 export default function CategoryTest() {
-  getCategory({ })
-    .then((response) => console.log(response))
-    .catch((error) => console.dir(error))
+  const [category, setcategory] = useState([])
 
-  // writeCategory({ name: "react" })
-  //   .then((response) => console.log(response))
-  //   .catch((error) => console.dir(error))
+  // * 전체 카테고리 가져오기
+  const getCategory = (id) => {
+    CategoryAPI.get({ id })
+      .then((response) => {
+        setcategory([...response.data])
+      })
+      .catch((error) => console.warn("response" in error ? error.response.data.message : error))
+  }
 
-  // updateCategory({ id: 13, name: "reactUpdate" })
-  //   .then((response) => console.log(response))
-  //   .catch((error) => console.dir(error))
+  // * 카테고리 작성
+  const writeCategory = (name, order, isFavorited) => {
+    CategoryAPI.write({ name, order, isFavorited })
+      .then((response) => {
+        setcategory(m => m.concat(response.data))
+      })
+      .catch((error) => console.warn("response" in error ? error.response.data.message : error))
+  }
 
-  // deleteCategory({ id: 13 })
-  //   .then((response) => console.log(response))
-  //   .catch((error) => console.dir(error))
+  // * 카테고리 수정
+  const updateCategory = (id, name, order, isFavorited) => {
+    CategoryAPI.update({ id, name, order, isFavorited })
+      .then(() => {
+        // * 전체 카테고리 가져오기
+        getCategory()
+      })
+      .catch((error) => console.warn("response" in error ? error.response.data.message : error))
+  }
+
+  // * 카테고리 삭제
+  const deleteCategory = (id) => {
+    CategoryAPI.remove({ id })
+      .then((response) => {
+        if (response.status === 204) {
+          getCategory()
+        }
+        else throw new Error("서버 에러")
+      })
+      .catch((error) => console.warn("response" in error ? error.response.data.message : error))
+  }
+
+  useEffect(() => {
+    // getCategory()
+    // writeCategory("react hi~",1)
+    // updateCategory(8,"update~",1)
+    // deleteCategory(31)
+
+  }, []);
 
   return (
     <div>
       this is Category TEST PAGE
+      {[].concat(category).map((data, id) => <div key={data.id}>{data.name} : {data.id}</div>)}
     </div>
   )
 }
+
