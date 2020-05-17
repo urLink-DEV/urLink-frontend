5ab608489dce37baf809b14280028a781f368d14
leesoo7595 <isoo7510@gmail.com>
2020-05-17 23:06:28 +0900

    :sparkles: implement category history drawer

--- src/components/CategoryDrawer.js
+++ src/components/CategoryDrawer.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import IconButton from '@material-ui/core/IconButton';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import MenuIcon from '@material-ui/icons/Menu';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import { CategoryTab } from '../components/category';
-import '../pages/Category.scss';
+import React from 'react'
+import Drawer from '@material-ui/core/Drawer'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import { CategoryTab } from '../components/category'
+import '../pages/Category.scss'
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
+    height: '100vh',
   },
   drawer: {
     [theme.breakpoints.up('sm')]: {
@@ -54,15 +48,9 @@ export default function CategoryDrawer(props) {
   console.log(window, categories, favoriteCategories)
   const classes = useStyles();
   const theme = useTheme();
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
 
   const drawer = (
     <div>
-      <div className={classes.toolbar} />
       <div className="list-tab-layout">
         <div className="favorite-text">
           Favorite
@@ -99,58 +87,22 @@ export default function CategoryDrawer(props) {
 
   return (
     <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            className={classes.menuButton}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            Responsive drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
       <nav className={classes.drawer} aria-label="mailbox folders">
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Hidden smUp implementation="css">
-          <Drawer
-            container={container}
-            variant="temporary"
-            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
-          >
-            {drawer}
-          </Drawer>
-        </Hidden>
-        <Hidden xsDown implementation="css">
-          <Drawer
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            variant="permanent"
-            open
-          >
-            {drawer}
-          </Drawer>
-        </Hidden>
+        <Drawer
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          variant="permanent"
+          open
+        >
+          {drawer}
+        </Drawer>
       </nav>
       <main className={classes.content}>
         <div className={classes.toolbar} />
         {props.children}
       </main>
+      {props.appBar}
     </div>
   );
 }
--- src/pages/CategoryTestPage.js
+++ src/pages/CategoryTestPage.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import CategoryDrawer from '../components/CategoryDrawer';
-import CategoryCard from '../components/CategoryCard';
-import './Category.scss';
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import CategoryAppBar from '../components/CategoryAppBar'
+import CategoryDrawer from '../components/CategoryDrawer'
+import CategoryCard from '../components/CategoryCard'
+import './Category.scss'
 
 
 export default function CategoryPage(props) {
@@ -10,26 +11,25 @@ export default function CategoryPage(props) {
   const {
     getCategories,
     getFavoriteCategories,
-    getCategoryUrlInfoList
-  } = props;
+    getCategoryUrlInfoList,
+  } = props
 
   console.log('categoryPage', getCategories, getFavoriteCategories, getCategoryUrlInfoList)
 
   return (
-    <div>
-      <CategoryDrawer 
-        categories={getCategories}
-        favoriteCategories={getFavoriteCategories}
-      >
-        <Grid container spacing={2}>
-          {getCategoryUrlInfoList.map(urlObj => 
-            <Grid item xs={2}>
-              <CategoryCard urlInfoList={urlObj} />
-            </Grid>
-          )}
-        </Grid>
-      </CategoryDrawer>
-    </div>
+    <CategoryDrawer
+      appBar={<CategoryAppBar>helllo</CategoryAppBar>}
+      categories={getCategories}
+      favoriteCategories={getFavoriteCategories}
+    >
+      <Grid container spacing={2}>
+        {getCategoryUrlInfoList.map(urlObj => 
+          <Grid item xs={2}>
+            <CategoryCard urlInfoList={urlObj} />
+          </Grid>
+        )}
+      </Grid>
+    </CategoryDrawer>
   )
 } 
 
