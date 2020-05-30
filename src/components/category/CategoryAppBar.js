47e7a1923f913502d9a6ec80118af2d20435cd43
leesoo7595 <isoo7510@gmail.com>
2020-05-30 19:07:39 +0900

    :sparkles: implement popover alarm, profile

--- src/components/category/CategoryAppBar.js
+++ src/components/category/CategoryAppBar.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react'
+import Button from '@material-ui/core/Button'
+import Popover from '@material-ui/core/Popover'
 import CategoryHistory from './CategoryHistory'
 import CategoryHistoryList from './CategoryHistoryList'
 import useStyles from './styles/CategoryAppBar'
@@ -11,23 +13,87 @@ export default function CategoryAppBar(props) {
   const classes = useStyles()
   const {urlList} = props
   const [openDrawer, setOpenDrawer] = useState(false)
+  const [anchorAlarm, setAnchorAlarm] = useState(null)
+  const [anchorProfile, setAnchorProfile] = useState(null)
+
   const onClickHistoryDrawer = () => {
     setOpenDrawer(!openDrawer)
   }
+
+  const handleAlarmPopOverClick = (event) => {
+    console.log(event.currentTarget)
+    setAnchorAlarm(event.currentTarget)
+  };
+
+  const handleAlarmPopOverClose = () => {
+    setAnchorAlarm(null)
+  };
+
+  const handleProfilePopOverClick = (event) => {
+    console.log(event.currentTarget)
+    setAnchorProfile(event.currentTarget)
+  };
+
+  const handleProfilePopOverClose = () => {
+    setAnchorProfile(null)
+  };
+
+  const alarmOpen = Boolean(anchorAlarm)
+  const alarmId = alarmOpen ? 'alarm-popover' : undefined
+  const profileOpen = Boolean(anchorProfile)
+  const profileId = profileOpen ? 'profile-popover' : undefined
 
   return (
     <div>
       <div className={classes.appBar}>
         <div className="drawer-btn-group">
-          <button onClick={onClickHistoryDrawer}>
+          <Button onClick={onClickHistoryDrawer}>
             <img src={history} alt="history button" />
-          </button>
-          {/* <button onClick={onClickAlarmBtn}>
+          </Button>
+          <Button
+            aria-describedby={alarmId}
+            onClick={handleAlarmPopOverClick}
+          >
             <img src={alarm} alt="alarm button" />
-          </button>
-          <button onClick={onClickProfileBtn}>
-            <img src={person} alt="person button" /> */}
-          {/* </button> */}
+          </Button>
+          <Popover 
+            id={alarmId}
+            open={alarmOpen}
+            onClose={handleAlarmPopOverClose}
+            anchorEl={anchorAlarm}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            transformOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+          >
+            asdfasdf
+          </Popover>
+          <Button
+            aria-describedby={profileId}
+            onClick={handleProfilePopOverClick}
+          >
+            <img src={person} alt="person button" />
+          </Button>
+          <Popover 
+            id={profileId}
+            open={profileOpen}
+            onClose={handleProfilePopOverClose}
+            anchorEl={anchorProfile}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            transformOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+          >
+            asdfasdf
+          </Popover>
         </div>
       </div>
       <CategoryHistory open={openDrawer} onClickHistoryDrawer={onClickHistoryDrawer}>
