1a656b033d975dc65f37c7a4cf86d9828a3b5733
yjkwon07 <dudwn9073@nate.com>
2020-06-21 13:00:49 +0900

    manifest & bacground [U] if new Tab, dont  work default page change urLink - just one more new Tab what urLink Page (extensition)

--- public/background.js
+++ public/background.js
@@ -1,3 +1,6 @@
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//     chrome.tabs.create({ url: chrome.extension.getURL('index.html'), selected: true });
-// });
+chrome.tabs.onCreated.addListener(function (_tab) {
+  chrome.tabs.query({ currentWindow: true }, function (tabs) {
+    // chrome.tabs.get(tabs[0].id,  (s) => {console.log(s)})
+    if (tabs.length <= 1) chrome.tabs.create({ url: chrome.extension.getURL('index.html'), selected: true });
+  });
+});
