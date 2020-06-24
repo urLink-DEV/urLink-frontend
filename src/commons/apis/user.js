b4313b928dfcfaa8c1cb64e3d488382ec78e7c78
yjkwon07 <dudwns9073@nate.com>
2020-06-25 01:06:17 +0900

    login, singup container [U] - user API [A]   - login   - register

--- src/commons/apis/user.js
+++ src/commons/apis/user.js
@@ -1,6 +1,6 @@
 import { axios, api } from '../http'
 import queryData from '../queryData'
-import { getDashQueryParams } from '../queryParam'
+import { getDashQueryParams } from '../quryParam'
 
 const userAPI = {
   get : ({ id }) => {
@@ -12,6 +12,61 @@ const userAPI = {
     } catch (error) {
       console.warn(error)
     }
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  nRegister: ({ email, username, password }) => {
+    try {
+      const nRegister = queryData["n_register"]
+      nRegister.email = email
+      nRegister.username = username
+      nRegister.password = password
+      return axios.post(api.N_MEMBER_REGISTER, nRegister)
+    } catch (error) {
+      console.warn(error)
+    }
+  },
+
+  nLogin: ({ email, password }) => {
+    try {
+      const nLogin = queryData["n_login"]
+      nLogin.email = email
+      nLogin.password = password
+      return axios.post(api.N_MEMBER_LOGIN, nLogin)
+    } catch (error) {
+      console.warn(error)
+    }
+  },
+
+  gRegister: ({ token }) => {
+    const gRegister = queryData['g_register']
+    gRegister.token = token
+    return axios.post(api.G_MEMBER_REGISTER, gRegister)
+  },
+
+  gLogin: ({ token }) => {
+    const gLogin = queryData['g_login']
+    gLogin.token = token
+    return axios.post(api.G_MEMBER_LOGIN, gLogin)
   }
 }
 export default userAPI
