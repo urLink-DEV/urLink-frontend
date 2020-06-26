eb42365658d15a85cb9c59bb1587604622e4d8ff
yjkwon07 <dudwns9073@nate.com>
2020-06-26 22:21:17 +0900

    alarmSocketAPI [A]

--- src/components/popover/AlarmPopover.js
+++ src/components/popover/AlarmPopover.js
@@ -1,5 +1,5 @@
 /* global chrome */
-import React from 'react'
+import React, {useEffect} from 'react'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
@@ -10,34 +10,47 @@ import Avatar from '@material-ui/core/Avatar'
 import ImageIcon from '@material-ui/icons/Image'
 import CloseIcon from '@material-ui/icons/Close'
 import useStyles from './styles/AlarmPopover'
-
+/*
+ * id: 15
+ * name: "test"
+ * alarm_has_done: false
+ * alarm_has_read: false
+ * reserved_time: "2020-06-21 09:31:00+00:00"
+ * url_description: "개발을 진행하거나, 웹브라우저를 이용하여 업무를 하다보면, 의외로 크롬에 있는 웹스토어에서 extension을 다운받아 진행하는 경우들이 많습니다. 웹브라우저내의 스크린샷을 찍는다"
+ * url_favicon_path: "https://trustyoo86.github.io/assets/icons/android-icon-192x192.png"
+ * url_image_path: "https://trustyoo86.github.io/assets/back-code2.jpg"
+ * url_path: "https://trustyoo86.github.io/javascript/2019/12/27/chrome-extension-overview.html"
+ * url_title: "Chrome extension 만들기 (1)" 
+ */
 export default function AlarmPopover(props) {
   const classes = useStyles()
-  const {list} = props
+  const {alarmList, onalarmRead, onNoReturnAlarm} = props
 
-  const onClickAlarm = () => {
-    console.log('click Alarm')
+  const onClickAlarm = (id) => {
+    onalarmRead(id)
   }
 
-  const onDeleteAlarm = () => {
-    console.log('delete alarm')
+  const onDeleteAlarm = (id) => {
+    onNoReturnAlarm(id)
   }
+
+  console.log(alarmList)
 
   return (
     <List className={classes.root}>
-      {list.map((e, i) => 
-        <ListItem key={i} button onClick={onClickAlarm}>
+      {alarmList.map((alarm) => 
+        <ListItem key={alarm.id} button onClick={() => onClickAlarm(alarm.id)}>
         <ListItemAvatar>
           <Avatar className={classes.avatar}>
-            {e.img ? <img className={classes.img} src={e.img}/> : <ImageIcon />}
+            {alarm.img ? <img className={classes.img} src={alarm.img}/> : <ImageIcon />}
           </Avatar>
         </ListItemAvatar>
         <ListItemText className={classes.text} 
-          primary={e.title} 
-          secondary={e.date}
+          primary={alarm.title} 
+          secondary={alarm.date}
         />
         <ListItemSecondaryAction>
-            <IconButton edge="end" aria-label="comments" onClick={onDeleteAlarm}>
+            <IconButton edge="end" aria-label="comments" onClick={() => onDeleteAlarm(alarm.id)}>
               <CloseIcon />
             </IconButton>
           </ListItemSecondaryAction>
