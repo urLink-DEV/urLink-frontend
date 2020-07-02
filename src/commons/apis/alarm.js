31ea8ff21e4175a9ab81cea7a0d582b9d1f6fb6f
yjkwon07 <dudwns9073@nate.com>
2020-07-03 04:52:49 +0900

    API parameter -> object [U] - params -> Object entries -> get queryData => Don't get undefined value

--- src/commons/apis/alarm.js
+++ src/commons/apis/alarm.js
@@ -3,37 +3,49 @@ import queryData from '../queryData'
 import { getQueryParams, getDashQueryParams } from '../quryParam'
 
 const alarmAPI = {
-  get : () => {
+  get : (alarmInfo) => {
     try {
-      const alarmRead = queryData["alarmRead"]
+      const alarmReadeKeys = Object.keys(queryData["alarmRead"])
+      const alarmRead = {}
+      Object.entries(alarmInfo).forEach(([key, value]) => {
+        if (alarmReadeKeys.includes(key)) alarmRead[key] = value
+      })
       return axios.get(api.ALARM, alarmRead)
     } catch (error) {
-      console.warn(error)
+      return {error}
     }
   },
 
-  write: ({ name, category, url, year, month, day, hour, minute }) => {
+  write: (alarmInfo) => {
     try {
+      const { category, url, reserved_time: { year, month, day } } = alarmInfo
       const queryParams = getQueryParams({ category, url })
       if (!queryParams) throw new Error(`category: ${category}, url: ${url} Id는 필수 입니다.`)
       else if(!year || !month || !day) throw new Error(`알람 필수 값을 넣어야 합니다. year : ${year} || month : ${month} || day : ${day}`)
-      const alarmWrite = Object.assign(queryData["alarmWrite"])
-      alarmWrite.name = name
-      alarmWrite.reserved_time = { year, month, day, hour, minute }
+      const alarmWriteKeys = Object.keys(queryData["alarmWrite"])
+      const alarmWrite = {}
+      Object.entries(alarmInfo).forEach(([key, value]) => {
+        if (alarmWriteKeys.includes(key)) alarmWrite[key] = value
+      })
       return axios.post(api.ALARM + queryParams, alarmWrite)
     } catch (error) {
-      console.warn(error)
+      return {error}
     }
   },
 
-  remove : ({ id }) => {
+  remove : (alarmInfo) => {
     try {
+      const { id } = alarmInfo
       const dashQueryParams = getDashQueryParams([id])
       if(!dashQueryParams) throw new Error(`alarm : ${id} Id는 필수 입니다.`)
-      const alarmDelete = Object.assign(queryData["alarmDelete"])
+      const alarmDeleteKeys = Object.keys(queryData["alarmDelete"])
+      const alarmDelete = {}
+      Object.entries(alarmInfo).forEach(([key, value]) => {
+        if (alarmDeleteKeys.includes(key)) alarmDelete[key] = value
+      })
       return axios.delete(api.ALARM + dashQueryParams, alarmDelete)
     } catch (error) {
-      console.warn(error)
+      return {error}
     }
   }
 }
