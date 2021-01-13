89057988b136f6b12be8e7da7154b9d6f7d6e35c
anxiubin <tnqls0120@naver.com>
2021-01-13 19:03:21 +0900

    :zap: ui dialog 모듈 개선 및 alertModal에 적용

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,12 +3,14 @@ import { fork, all } from "redux-saga/effects"
 import { pendingReducer } from "./pending"
 import { errorReducer, errorSaga } from "./error"
 import { categoryReducer, categorySaga } from "./category"
+import { uiReducer } from "./ui"
 
 // root reducer
 export const rootReducer = combineReducers({
 	pending: pendingReducer,
 	error: errorReducer,
 	category: categoryReducer,
+	ui: uiReducer,
 })
 
 // root saga
--- src/modules/ui/hooks/useDialog.js
+++ src/modules/ui/hooks/useDialog.js
@@ -2,28 +2,30 @@ import { useCallback } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { openDialog, closeDialog, closeAllDialogs } from "@modules/ui"
 
-const useDialog = () => {
-	const dispatch = useDispatch()
+const useDialog = (type) => {
 	const dialogs = useSelector((state) => state.ui.dialogs)
-	const open = useCallback(
-		(type, props) => {
-			dispatch(openDialog({ type, props }))
-		},
-		[dispatch]
-	)
-	const close = useCallback(
-		(type) => {
+	const dispatch = useDispatch()
+	const open = !!dialogs.find((dialog) => dialog.type === type)
+
+	const toggle = useCallback(() => {
+		if (open) {
 			dispatch(closeDialog({ type }))
-		},
-		[dispatch]
-	)
+		} else {
+			dispatch(openDialog({ type }))
+		}
+	}, [open, type, dispatch])
+
+	const close = useCallback(() => {
+		dispatch(closeDialog({ type }))
+	}, [dispatch, type])
+
 	const closeAll = useCallback(() => {
 		if (dialogs.length) {
 			dispatch(closeAllDialogs())
 		}
 	}, [dispatch, dialogs])
 
-	return { open, close, closeAll }
+	return { open, toggle, close, closeAll }
 }
 
 export default useDialog
