19c1f2a737fbb59353e173a3a02b051e571672eb
leesoo <isoo7510@gmail.com>
2021-01-29 17:42:21 +0900

    :lipstick: 카드 Search 버튼 스타일링 수정

--- src/components/category/CategoryDrawer.js
+++ src/components/category/CategoryDrawer.js
@@ -8,13 +8,15 @@ import {useLinkState, useLinkDispatch, useCategoryState, useCategoryDispatch} fr
 // * UI (CSS)
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
+import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
 import ToggleButton from '@material-ui/lab/ToggleButton'
-import InputBase from '@material-ui/core/InputBase'
 import CreateIcon from '@material-ui/icons/Create'
+import InputBase from '@material-ui/core/InputBase'
 import {useStyles, StyledToggleButtonGroup} from './styles/CategoryDrawer'
+import SearchIcon from '@material-ui/icons/Search'
 
-import SearchIcon from '../../images/search.png'
+// import SearchIcon from '../../images/search.png'
 import CategoryEmptyIcon from '../../images/group-5.svg'
 import linkListEmptyIcon from '../../images/group-11.png'
 import linkListSearchEmptyIcon from '../../images/group-17.png'
@@ -26,7 +28,6 @@ import CategoryAppBar from './CategoryAppBar'
 import CategorySearchPopOver from './CategorySearchPopOver'
 import Snackbar from '../Snackbar'
 import useEventListener from '../../hooks/useEventListener'
-
 export default function CategoryDrawer(props) {
 
   const classes = useStyles()
@@ -134,48 +135,76 @@ export default function CategoryDrawer(props) {
   }
 
   const searchPopOverBtn = (
-    <CategorySearchPopOver>
-      <Grid  className={classes.popover}>
-        <Grid className={classes.popoverDiv}>
-          <img  className={classes.searchIcon}
-            alt='search icon'
-            src={SearchIcon}
-            />
-          <span className={classes.searchBtnText}>
-            Search
-          </span>
-        </Grid>
-        <Grid>
-          <input className={classes.textfield}
-            placeholder="검색어를 입력해 주세요."
-            onKeyDown={handlePressEnterSearchValue}
-          />
-        </Grid>
-        <Grid>
-          <StyledToggleButtonGroup 
-            size="small"
-            value={toggleAlignment}
-            exclusive
-            onChange={handleToggleChange}
-          >
-            <ToggleButton className={classes.popoverBtn}
-              value='left'
-              variant='contained'
-              size='small'
-            >
-              도메인
-            </ToggleButton>
-            <ToggleButton className={classes.popoverBtn}
-              value='right'
-              variant='contained'
-              size='small'
-            >
-              단어
-            </ToggleButton>
-          </StyledToggleButtonGroup>
-        </Grid>
-      </Grid> 
-    </CategorySearchPopOver>
+    <div className={classes.search}>
+      <div className={classes.searchIcon}>
+        <SearchIcon />
+      </div>
+      <InputBase
+        placeholder="Search…"
+        classes={{
+          root: classes.inputRoot,
+          input: classes.inputInput,
+        }}
+        inputProps={{ 'aria-label': 'search' }}
+      />
+    </div>
+    // <CategorySearchPopOver>
+    //   <Grid  className={classes.popover}>
+    //     <Grid className={classes.popoverDiv}>
+    //       {/* <img  className={classes.searchIcon}
+    //         alt='search icon'
+    //         src={SearchIcon}
+    //         />
+    //       <span className={classes.searchBtnText}>
+    //         Search
+    //       </span> */}
+    //       <div className={classes.search}>
+    //         <div className={classes.searchIcon}>
+    //         <img  className={classes.searchIcon}
+    //           alt='search icon'
+    //           src={SearchIcon} />
+    //         </div>
+    //         <InputBase
+    //           placeholder="Search…"
+    //           classes={{
+    //             root: classes.inputRoot,
+    //             input: classes.inputInput,
+    //           }}
+    //           inputProps={{ 'aria-label': 'search' }}
+    //         />
+    //       </div>
+    //     </Grid>
+    //     <Grid>
+    //       <input className={classes.textfield}
+    //         placeholder="검색어를 입력해 주세요."
+    //         onKeyDown={handlePressEnterSearchValue}
+    //       />
+    //     </Grid>
+    //     <Grid>
+    //       <StyledToggleButtonGroup 
+    //         size="small"
+    //         value={toggleAlignment}
+    //         exclusive
+    //         onChange={handleToggleChange}
+    //       >
+    //         <ToggleButton className={classes.popoverBtn}
+    //           value='left'
+    //           variant='contained'
+    //           size='small'
+    //         >
+    //           도메인
+    //         </ToggleButton>
+    //         <ToggleButton className={classes.popoverBtn}
+    //           value='right'
+    //           variant='contained'
+    //           size='small'
+    //         >
+    //           단어
+    //         </ToggleButton>
+    //       </StyledToggleButtonGroup>
+    //     </Grid>
+    //   </Grid> 
+    // </CategorySearchPopOver>
   )
 
   // TODO Refactoring
@@ -241,54 +270,50 @@ export default function CategoryDrawer(props) {
         editCategoryTitle={editCategoryTitle}
       />
       <main className={classes.content}>
-        <Grid container className={classes.toolbar}>
-          {
-            categories.length !== 0
-              ? isEditCategoryTitle
-              ? <Grid item>
-                  <InputBase className={classes.mainFont}
+        <div position="static">
+          <Toolbar container className={classes.toolbar}>
+            <div className={classes.title}>
+            {
+              categories.length !== 0
+                ? isEditCategoryTitle
+                ? <InputBase className={classes.mainFont}
                     onChange={handleChangeEditCategoryTitle}
                     onClick={handleClickEditCategoryTitle}
                     onKeyUp={handleKeyupEditCategoryTitle}
                     value={editCategoryTitle}
                   />
-                </Grid>
-              : <>
-                <Grid item>
+                : <>
                   <button className={classes.mainFont} 
                     onClick={handleClickCategoryTitle}>
                     {selectedCategory.name}
                   </button>
-                </Grid>
-                <Grid item>
-                  <IconButton className={classes.settingsIcon}
+                  <IconButton
                     aria-label="setting"
                     onClick={handleClickEditTitle}
                   >
                     <CreateIcon fontSize="small" />
                   </IconButton>
-                </Grid>
-              </> : null
-          }
-          <Grid item>
+                </> : null
+            }
+            </div>
             {categories.length !== 0 ? searchPopOverBtn : null}
-          </Grid>
-          {
-            (selectedCardList.length > 0) 
-            ? <Grid>
-                <Button onClick={handleClickOpenSelectedCardList}>
-                  탭 열기
-                </Button>
-                <Button color="secondary" 
-                  onClick={handleClickDeleteSelectedCardList}
-                >
-                  탭 삭제
-                </Button>
-              </Grid> 
-            : null
-          }
-        </Grid>
-        <Grid container>
+            {
+              (selectedCardList.length > 0) 
+              ? <>
+                  <Button onClick={handleClickOpenSelectedCardList}>
+                    탭 열기
+                  </Button>
+                  <Button color="secondary" 
+                    onClick={handleClickDeleteSelectedCardList}
+                  >
+                    탭 삭제
+                  </Button>
+                </>
+              : null
+            }
+          </Toolbar>
+        </div>
+        <div container>
           { 
             links.length ? links?.map((linkObj, idx) => 
               <Grid item key={idx} className={classes.gridCard}>
@@ -315,7 +340,7 @@ export default function CategoryDrawer(props) {
               alt='link list empty'
             />
           }
-        </Grid>
+        </div>
         <Snackbar open={deleteSuccessAlert}
           alertText="선택하신 카드가 삭제되었습니다."
           handleClose={handleDeleteSuccessAlertClose}
