e75273a29145231d5ce66ae3a07028ae70a6e810
yjkwon07 <dudwns9073@nate.com>
2021-01-29 18:17:18 +0900

    💄 DragHistoryList, useOutsideAlerter - ScrollUpButton 추가 - ScrollUpButton, DragBox 부모 안에 absoulte로 설정 - useOutsiderAlerter prettier 정리

--- src/hooks/useOutsideAlerter.js
+++ src/hooks/useOutsideAlerter.js
@@ -1,25 +1,16 @@
-import { useEffect } from "react"
+import { useEffect } from 'react';
 
-/**
- * Hook that alerts clicks outside of the passed ref
- */
-export default function useOutsideAlerter(ref, bool, callback) {
-  function handleClickOutside(event) {
-
-    if (ref.current && !ref.current.contains(event.target)) {
-      return callback()
-    }
-  }
-
+function useOutsideAlerter(ref, bool, callback) {
   useEffect(() => {
-    // Bind the event listener
-    if (bool) {
-      document.addEventListener("mousedown", handleClickOutside)
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current?.contains(event.target)) {
+        return callback();
+      }
     }
-    
-    return () => {
-        // Unbind the event listener on clean up
-      document.removeEventListener("mousedown", handleClickOutside)
-    }
-  }, [ref, bool])
+
+    if (bool) document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, [ref, bool, callback]);
 }
+
+export default useOutsideAlerter;
--- src/pages/category/AppBar/DragableHistoryList/index.jsx
+++ src/pages/category/AppBar/DragableHistoryList/index.jsx
@@ -2,8 +2,9 @@ import React, { Fragment, useRef, useState, useEffect, useCallback } from 'react
 import { debounce } from 'lodash';
 import clsx from 'clsx';
 import { Card, CardContent, CardMedia, IconButton, List, Typography } from '@material-ui/core';
-import RefreshIcon from '@material-ui/icons/Refresh';
+import { Refresh as RefreshIcon } from '@material-ui/icons';
 import SearchButton from '@components/SearchButton';
+import ScrollUpButton from '@components/ScrollUpButton';
 import useStyles from './style';
 import HistoryDragBox from './HistoryDragBox';
 import HistoryDateTitle from './HistoryDateTitle';
@@ -19,14 +20,16 @@ const { LINK_DROP_ZONE } = DROP_ZONE;
 
 function DragableHistoryList() {
   const classes = useStyles();
-  const { open, toggle, close } = useDropZone(LINK_DROP_ZONE);
-  const historyListRef = useRef(null);
-  const dragBoxRef = useRef(null);
   const [selectedList, setSelectedList] = useState([]);
   const { filter, listData, reload, search, next } = useHistoryLinkListData();
   const { setDragData, clearDragData } = useDrag(LINK);
+  const { open, toggle, close } = useDropZone(LINK_DROP_ZONE);
+  const [buttonOpen, setButtonOpen] = useState(null);
+  const historyRootRef = useRef(null);
+  const historyContentRef = useRef(null);
+  const dragBoxRef = useRef(null);
   useOutsideAlerter(
-    historyListRef,
+    historyRootRef,
     !!selectedList.length,
     useCallback(() => {
       setSelectedList([]);
@@ -39,7 +42,7 @@ function DragableHistoryList() {
     const isSelectedItem = selectedList.find((selected) => selected.id === id);
     if (!isSelectedItem) {
       setSelectedList((listData) => listData.concat({ id, path }));
-      dragListData.concat({ id, path });
+      dragListData.push({ id, path });
     }
     setDragData(dragListData);
     e.dataTransfer.setDragImage(dragBoxRef.current, 110, 35);
@@ -57,24 +60,31 @@ function DragableHistoryList() {
     else setSelectedList((listData) => listData.concat({ id, path }));
   };
 
-  const handleHistorySearchScroll = (e) => {
+  const handleHistoryListScroll = (e) => {
     const scrollTop = e.currentTarget.scrollTop;
     const scrollHeight = e.currentTarget.scrollHeight;
     const clientHeight = e.currentTarget.clientHeight;
+    setButtonOpen(scrollTop + clientHeight / 2 > clientHeight);
     if (Math.ceil(scrollTop + clientHeight) >= scrollHeight) next();
   };
 
   const handleHistorySearch = (e) => {
     const { keyCode } = e;
     const { value } = e.currentTarget;
-    if (keyCode === 13) search(value);
+    if (keyCode === 13) {
+      historyContentRef.current.scrollTop = 0;
+      search(value);
+    }
   };
 
   const handleOpenNewTab = (_e) => {
     createTabList(selectedList.reduce((list, link) => list.concat(link.path), []));
   };
 
-  const handleReload = debounce(reload, 400);
+  const handleReload = debounce(() => {
+    historyContentRef.current.scrollTop = 0;
+    reload();
+  }, 400);
 
   useEffect(() => {
     if (!open && selectedList.length) toggle();
@@ -82,70 +92,75 @@ function DragableHistoryList() {
   }, [selectedList, toggle, close, open]);
 
   return (
-    <>
-      <HistoryDragBox ref={dragBoxRef} selectedCount={selectedList.length} />
-      <Card className={classes.root} onScroll={handleHistorySearchScroll}>
-        <CardContent>
-          <div className={classes.header}>
-            <div className={classes.spread}>
-              <Typography className={classes.mainText}>방문기록</Typography>
-              <div className={classes.center}>
-                <SearchButton
-                  inputProps={{
-                    defaultValue: filter.text,
-                    onKeyDown: handleHistorySearch,
-                  }}
-                />
-                {!!selectedList.length && (
-                  <button className={classes.tabOpenButton} onClick={handleOpenNewTab}>
-                    <span className={classes.tabOpenText}>탭 열기 ({selectedList.length})</span>
-                  </button>
-                )}
-              </div>
-            </div>
-            <IconButton onClick={handleReload}>
-              <RefreshIcon />
-            </IconButton>
+    <Card ref={historyRootRef} className={classes.root}>
+      <div className={classes.header}>
+        <div className={classes.rowSpread}>
+          <Typography className={classes.mainText}>
+            방문기록
+          </Typography>
+          <div className={classes.center}>
+            <SearchButton
+              inputProps={{
+                defaultValue: filter.text,
+                onKeyDown: handleHistorySearch,
+              }}
+            />
+            {!!selectedList.length && (
+              <button className={classes.tabOpenButton} onClick={handleOpenNewTab}>
+                <span className={classes.tabOpenText}>탭 열기 ({selectedList.length})</span>
+              </button>
+            )}
           </div>
-          {!!listData.length ? (
-            <List ref={historyListRef}>
-              {listData.map((data) => (
-                <Fragment key={data.id}>
-                  <HistoryDateTitle data={data} />
-                  <History
-                    draggable
-                    data-type="link"
-                    data={data}
-                    isSelected={selectedList.find((list) => list.id === data.id)}
-                    onDragStart={handleDragStart({ id: data.id, url: data.url })}
-                    onDragEnd={handleDragEnd()}
-                    onClick={handleToogleSelectIem({ id: data.id, url: data.url })}
-                  />
-                </Fragment>
-              ))}
-            </List>
-          ) : filter.text ? (
-            <div className={clsx(classes.center, classes.marginTop16)}>
-              <CardMedia
-                component="img"
-                className={classes.imgContent}
-                image={linkListSearchEmptyImg}
-                alt="link search list is empty"
-              />
-            </div>
-          ) : (
-            <div className={clsx(classes.center, classes.marginTop16)}>
-              <CardMedia
-                component="img"
-                className={classes.imgContent}
-                image={linkListEmptyImg}
-                alt="link list is empty"
-              />
-            </div>
-          )}
-        </CardContent>
-      </Card>
-    </>
+        </div>
+        <IconButton onClick={handleReload}>
+          <RefreshIcon />
+        </IconButton>
+      </div>
+      <CardContent
+        ref={historyContentRef}
+        className={classes.content}
+        onScroll={handleHistoryListScroll}
+      >
+        {!!listData.length ? (
+          <List>
+            {listData.map((data) => (
+              <Fragment key={data.id}>
+                <HistoryDateTitle data={data} />
+                <History
+                  draggable
+                  data-type={LINK}
+                  data={data}
+                  isSelected={selectedList.find((list) => list.id === data.id)}
+                  onDragStart={handleDragStart({ id: data.id, url: data.url })}
+                  onDragEnd={handleDragEnd()}
+                  onClick={handleToogleSelectIem({ id: data.id, url: data.url })}
+                />
+              </Fragment>
+            ))}
+          </List>
+        ) : filter.text ? (
+          <div className={clsx(classes.center, classes.marginTop16)}>
+            <CardMedia
+              component="img"
+              className={classes.imgContent}
+              image={linkListSearchEmptyImg}
+              alt="link search list is empty"
+            />
+          </div>
+        ) : (
+          <div className={clsx(classes.center, classes.marginTop16)}>
+            <CardMedia
+              component="img"
+              className={classes.imgContent}
+              image={linkListEmptyImg}
+              alt="link list is empty"
+            />
+          </div>
+        )}
+      </CardContent>
+      <HistoryDragBox ref={dragBoxRef} selectedCount={selectedList.length} />
+      <ScrollUpButton className={classes.scrollUp} ref={historyContentRef} open={buttonOpen} />
+    </Card>
   );
 }
 
