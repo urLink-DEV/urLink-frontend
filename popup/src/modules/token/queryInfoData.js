e11b70bc0d34aad2204f27140441a2e6aa4f45eb
yjkwon07 <dudwns9073@nate.com>
2021-02-10 16:09:02 +0900

    ⚡️ popup > http 세팅 - http, modules - localstorage - axios intercepter

---
+++ popup/src/modules/token/queryInfoData.js
@@ -0,0 +1,29 @@
+const queryInfoData = {
+  /**
+   * * JWT 갱신 POST
+   * * /user/token/refresh/
+   * * JWT 불필요
+   */
+  updateToken: {
+    API: '/user/token/refresh/',
+    method: 'post',
+    bodyQuery: {
+      refresh: '',
+    },
+  },
+
+  /**
+   * * JWT 검사 POST
+   * * /user/token/verify/
+   * * JWT 불필요
+   */
+  checkToken: {
+    API: '/user/token/verify/',
+    method: 'post',
+    bodyQuery: {
+      token: '',
+    },
+  },
+};
+
+export default queryInfoData;
