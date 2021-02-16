b508543a6abe3acd59804477e3c6ca55d9f2df8a
leesoo <isoo7510@gmail.com>
2021-02-16 23:29:01 +0900

    :construction:  Link 추가

--- src/pages/category/Main/index.jsx
+++ src/pages/category/Main/index.jsx
@@ -1,18 +1,50 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
 import LinkDropZone from './LinkDropZone'
 import Header from './Header'
+import Card from './Card'
 import useStyles from './style'
 
 function Main({title}) {
 
   const classes = useStyles();
-
+  const links = []
+  
   return (
     <>
       <LinkDropZone />
       <main className={classes.content}>
         <div position="static">
           <Header title={title} />
+        </div>
+        <div container>
+          { 
+            links.length ? links?.map((linkObj, idx) => 
+              <Grid item key={idx} className={classes.gridCard}>
+                {/* <CategoryCard key={idx}
+                  linkInfo={linkObj}
+                  handleSelectedCard={handleSelectedCard(linkObj)}
+                  isReset={isReset}
+                  setIsReset={setIsReset}
+                  writeAlarm={writeAlarm}
+                /> */}
+                <Card />
+              </Grid>) 
+            : categories.length === 0 ? 
+              <img className={classes.imgCenter} 
+                src={CategoryEmptyIcon} 
+                alt='category list empty'
+              />
+            :  searchValue ? 
+              <img className={classes.imgCenter}
+                src={linkListSearchEmptyIcon} 
+                alt='link list search empty'
+              />
+          : <img className={classes.imgCenter}
+              src={linkListEmptyIcon} 
+              alt='link list empty'
+            />
+          }
         </div>
       </main>
     </>
