5420b999b57dbad12d859ee4a0ecb7c584501f01
anxiubin <tnqls0120@naver.com>
2021-03-01 17:06:42 +0900

    ♻️CategoryItem 감싸고 있던 컴포넌트를 분리하여 CategoryItemWrapper 생성

---
+++ src/pages/Home/CategoryList/CategoryItemWrapper/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import useStyles from './style'
+
+function CategoryItemWrapper({ data, handleDragFunctions, draggedOrder, children }) {
+  const classes = useStyles()
+  const {
+    handleDragStart,
+    handleDragEnd,
+    handleDragOver,
+    handleDragLeave,
+    handleDragDrop,
+  } = handleDragFunctions
+
+  return (
+    <React.Fragment key={data.id}>
+      <div className={classes.dragline} />
+      <div
+        data-type="category"
+        draggable="true"
+        onDragStart={(e) => handleDragStart(e, data.id, data.name, data.order)}
+        onDragEnd={handleDragEnd}
+        onDragOver={(e) =>
+          handleDragOver(
+            e,
+            data.id,
+            draggedOrder < data.order ? data.order - 1 : data.order,
+            data.is_favorited
+          )
+        }
+        onDragLeave={handleDragLeave}
+        onDrop={handleDragDrop}
+      >
+        {children}
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default CategoryItemWrapper
