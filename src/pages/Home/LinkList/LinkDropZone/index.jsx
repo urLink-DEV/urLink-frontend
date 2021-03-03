aaf2250119d06da69480bf292316f59b76e3e3e8
anxiubin <tnqls0120@naver.com>
2021-03-04 03:11:18 +0900

    ♻️LinkDropZone 컴포넌트에서 handleDropOnCardArea 로직 수정

--- src/pages/Home/LinkList/LinkDropZone/index.jsx
+++ src/pages/Home/LinkList/LinkDropZone/index.jsx
@@ -1,45 +1,59 @@
-import React from 'react';
+import React, { useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import clsx from 'clsx';
-import { AddToPhotos as AddToPhotosIcon } from '@material-ui/icons';
-import useStyles from './style';
-import { DROP_ZONE, DRAG, useDrag, useDropZone } from '@modules/ui';
-import { linkCreateThunk } from '@modules/link'
-import { selectSelectedCategory } from '@modules/category'
+import clsx from 'clsx'
+import { AddToPhotos as AddToPhotosIcon } from '@material-ui/icons'
+import useStyles from './style'
+import { DROP_ZONE, DRAG, useDrag, useDropZone } from '@modules/ui'
+import { linkCreateThunk, linksRead } from '@modules/link'
+import { selectSelectedCategory, categoriesReadThunk } from '@modules/category'
 
-const { LINK } = DRAG;
-const { LINK_DROP_ZONE } = DROP_ZONE;
+const { LINK } = DRAG
+const { LINK_DROP_ZONE } = DROP_ZONE
 
 function LinkDropZone({ id }) {
-  const classes = useStyles();
+  const classes = useStyles()
   const dispatch = useDispatch()
   const selectedCategory = useSelector(selectSelectedCategory)
-  const { listData } = useDrag(LINK);
-  const { open } = useDropZone(LINK_DROP_ZONE);
+  const { listData } = useDrag(LINK)
+  const { open } = useDropZone(LINK_DROP_ZONE)
 
-  const dropOnCardArea = async (e) => {
+  const dispatchLinkCreate = useCallback(async () => {
+    const promises = []
+    listData.forEach((link) =>
+      promises.push(
+        dispatch(
+          linkCreateThunk({
+            categoryId: selectedCategory.id,
+            path: link.path,
+          })
+        )
+      )
+    )
+    return Promise.all(promises)
+  }, [dispatch, selectedCategory, listData])
+
+  const handleDropOnCardArea = async (e) => {
     e.stopPropagation()
-    listData.forEach(async link => await dispatch(linkCreateThunk({
-      categoryId: selectedCategory.id,
-      path: link.path
-    })))
-  };
+    await dispatchLinkCreate()
+    dispatch(categoriesReadThunk())
+    dispatch(linksRead.request({ categoryId: selectedCategory.id }))
+  }
 
-  const dragOverOnCardArea = (e) => {
-    e.preventDefault();
-  };
+  const handleDragOverOnCardArea = (e) => {
+    e.preventDefault()
+  }
 
   return (
     <div
       className={clsx(classes.coverBackground, {
         [classes.diplayNone]: !open,
       })}
-      onDrop={dropOnCardArea}
-      onDragOver={dragOverOnCardArea}
+      onDrop={handleDropOnCardArea}
+      onDragOver={handleDragOverOnCardArea}
     >
       <AddToPhotosIcon className={classes.addLinkIcon} />
     </div>
-  );
+  )
 }
 
-export default LinkDropZone;
+export default LinkDropZone
