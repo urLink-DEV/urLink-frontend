3128aae4d7ec7e938c772a46db5da0991bf1a8df
yjkwon07 <dudwns9073@nate.com>
2021-03-03 17:18:58 +0900

    💄 LinkList 컴포넌트 스타일 수정 - Header 부분 탭 열기/ 삭제, Search 구현 해야 함

--- src/pages/Home/LinkList/style.js
+++ src/pages/Home/LinkList/style.js
@@ -1,76 +1,17 @@
-import { fade, makeStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 
 export const useStyles = makeStyles((theme) => ({
-  root: {
-    '&::-webkit-scrollbar': {
-      display: 'none'
-    },
-    overflow: 'scroll',
-    display: 'flex',
-    height: '100vh',
-    backgroundColor: '#fafafa',
-  },
   content: {
     width: '100%',
     padding: theme.spacing(3),
     margin: '0 50px 0 0',
   },
-  imgCenter: {
-    position: 'absolute',
-    top: '50%',
-    left: '55%',
-    transform: 'translate(-45%, -55%)',
+  center: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 'calc(100vh - 112px);',
   },
-  popover: {
-    width: '220px',
-    borderRadius: '4px',
-    boxShadow: '0 2px 8px 0 rgba(0, 0, 0, 0.15), 0 5px 12px 0 rgba(0, 0, 0, 0.12)',
-    backgroundColor: '#ffffff',
-    fontFamily: "SpoqaHanSans",
-    padding: '5px 10px',
-  },
-  popoverDiv: {
-    marginBottom: 10,
-  },
-  popoverBtn: {
-    height: 30,
-    marginTop: 3
-  },
-  textfield: {
-    '&:focus' : {border: '1px solid #2083ff'},
-    outline: 'none',
-    borderRadius: '4px',
-    border: 'solid 1px #e9ecef',
-    backgroundColor: '#f1f3f5',
-    width: '100%',
-    height: '28px'
-  },
-  gridCard: {
-    padding: 0,
-    margin: 20,
-  },
-  // '@global': {
-  //   '.MuiFilledInput-inputMarginDense': {
-  //     paddingTop: '10px'
-  //   },
-  //   '.MuiToggleButton-root.Mui-selected': {
-  //     backgroundColor: '#2083ff',
-  //     color: 'white',
-  //   },
-  //   'div.makeStyles-root-283': {
-  //     right: 0
-  //   },
-  //   'div.makeStyles-drawerOpen-285': {
-  //     right: 50
-  //   },
-  //   '.MuiListItem-root': {
-  //     width: 208
-  //   },
-  //   '.MuiListItem-gutters' : {
-  //     padding: 0,
-  //     margin: '10px 0'
-  //   }
-  // }
 }))
 
 export default useStyles
