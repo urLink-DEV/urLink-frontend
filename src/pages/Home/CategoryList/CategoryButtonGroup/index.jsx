d5c2752ed20f01790cf712f4542ce3a6b64b0ead
yjkwon07 <dudwns9073@nate.com>
2021-03-06 17:19:50 +0900

    ♻️ CategoryButtonGroup - useCallback 추가 및 필요없는 import, 함수 사용 제거

--- src/pages/Home/CategoryList/CategoryButtonGroup/index.jsx
+++ src/pages/Home/CategoryList/CategoryButtonGroup/index.jsx
@@ -1,18 +1,15 @@
 import React, { useState, useCallback, useEffect } from 'react'
-import clsx from 'clsx'
 import { useDispatch, useSelector } from 'react-redux'
+import { AlertModal } from '@components/modals'
 import Button from '@material-ui/core/Button'
-import DeleteIcon from '@material-ui/icons/Delete'
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline'
 import Paper from '@material-ui/core/Paper'
 import InputBase from '@material-ui/core/InputBase'
-import { AlertModal } from '@components/modals'
-import useEventListener from '@hooks/useEventListener'
+import DeleteIcon from '@material-ui/icons/Delete'
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline'
 import useStyles from './style'
+import useEventListener from '@hooks/useEventListener'
 import { useDialog, useToast, MODAL_NAME } from '@modules/ui'
-
 import {
-  categorySelect,
   selectSelectedCategory,
   categoriesRead,
   categoryCreateThunk,
@@ -29,16 +26,15 @@ function CategoryButtonGroup() {
   const classes = useStyles()
   const dispatch = useDispatch()
   const { openToast } = useToast()
-  const selectedCategory = useSelector(selectSelectedCategory)
-
-  const [buttonState, setButtonState] = useState(BUTTON_STATE.addOpen)
-  const [categoryName, setCategoryName] = useState('')
-
   const {
     open: deleteCategoryOpen,
     toggle: deleteCategoryToggle,
     close: deleteCategoryClose,
   } = useDialog(MODAL_NAME.DELETE_CATEGORY_ALERT_MODAL)
+  const selectedCategory = useSelector(selectSelectedCategory)
+
+  const [buttonState, setButtonState] = useState(BUTTON_STATE.addOpen)
+  const [categoryName, setCategoryName] = useState('')
 
   const handleClickChangeToAddBtn = useCallback(() => {
     setButtonState(BUTTON_STATE.addOpen)
@@ -46,12 +42,12 @@ function CategoryButtonGroup() {
 
   useEventListener('click', handleClickChangeToAddBtn)
 
-  const handleClickOpenEnterTab = (e) => {
+  const handleClickOpenEnterTab = useCallback((e) => {
     e.stopPropagation()
     setButtonState(BUTTON_STATE.addInput)
-  }
+  },[])
 
-  const handleClickAdd = async (e) => {
+  const handleClickAdd = useCallback(async (e) => {
     try {
       e.stopPropagation()
       if (!categoryName) {
@@ -66,35 +62,31 @@ function CategoryButtonGroup() {
     } catch (error) {
       openToast({ type: 'error', message: error?.response?.data?.message || '네트워크 오류!!' })
     }
-  }
+  },[categoryName, dispatch, openToast])
 
   const handleChangeNewCategoryTitle = useCallback((e) => {
-    let checks = /[a-zA-Z]/
+    const checks = /[a-zA-Z]/
     if (checks.test(e.target.value)) {
       if (e.target.value.length >= 14) return
     } else if (e.target.value.length >= 7) return
     setCategoryName(e.target.value)
   }, [])
 
-  const onKeyUpAddTab = (e) => {
-    e.preventDefault()
-    e.stopPropagation()
+  const handleKeyUpAddTab = useCallback((e) => {
     if (e.keyCode === 13) handleClickAdd(e)
-  }
+  },[handleClickAdd])
 
-  const handleClickAddInput = (e) => {
-    e.preventDefault()
+  const handleClickAddInput = useCallback((e) => {
     e.stopPropagation()
-  }
+  },[])
 
-  const handleClickCancel = (e) => {
-    e.preventDefault()
+  const handleClickCancel = useCallback((e) => {
     e.stopPropagation()
     setButtonState(BUTTON_STATE.addOpen)
     setCategoryName('')
-  }
+  },[])
 
-  const handleClickdDelete = async (e) => {
+  const handleClickdDelete = useCallback(async (e) => {
     try {
       e.stopPropagation()
       await dispatch(categoryRemoveThunk({ id: selectedCategory.id }))
@@ -105,7 +97,7 @@ function CategoryButtonGroup() {
     } catch (error) {
       openToast({ type: 'error', message: error?.response?.data?.message || '네트워크 오류!!' })
     }
-  }
+  },[deleteCategoryToggle, dispatch, openToast, selectedCategory.id])
 
   useEffect(() => {
     if (selectedCategory?.type === 'mount') {
@@ -117,35 +109,27 @@ function CategoryButtonGroup() {
 
   return (
     <>
-      <Button
-        className={clsx(classes.addButton, {
-          [classes.hidden]: buttonState !== BUTTON_STATE.addOpen,
-          [classes.block]: buttonState === BUTTON_STATE.addOpen,
-        })}
-        variant="contained"
-        onClick={handleClickOpenEnterTab}
-      >
-        <AddCircleOutlineIcon className={classes.addCircleIcon} />
-      </Button>
-      <Button
-        className={clsx(classes.deleteButton, {
-          [classes.block]: buttonState === BUTTON_STATE.deleteOpen,
-          [classes.hidden]: buttonState !== BUTTON_STATE.deleteOpen,
-        })}
-        variant="contained"
-        onClick={deleteCategoryToggle}
-      >
-        <DeleteIcon style={{ color: '#cccccc' }} />
-      </Button>
+      {buttonState === BUTTON_STATE.addOpen && (
+        <Button className={classes.addButton} variant="contained" onClick={handleClickOpenEnterTab}>
+          <AddCircleOutlineIcon className={classes.addCircleIcon} />
+        </Button>
+      )}
+
+      {buttonState === BUTTON_STATE.deleteOpen && (
+        <Button className={classes.deleteButton} variant="contained" onClick={deleteCategoryToggle}>
+          <DeleteIcon style={{ color: '#cccccc' }} />
+        </Button>
+      )}
 
       {buttonState === BUTTON_STATE.addInput && (
         <Paper className={classes.enterTab} component="div">
           <InputBase
+            autoFocus
             className={classes.input}
             placeholder="New Category"
             value={categoryName}
             onChange={handleChangeNewCategoryTitle}
-            onKeyUp={onKeyUpAddTab}
+            onKeyUp={handleKeyUpAddTab}
             onClick={handleClickAddInput}
           />
           <Button className={classes.okBtn} onClick={handleClickAdd}>
@@ -168,4 +152,4 @@ function CategoryButtonGroup() {
   )
 }
 
-export default CategoryButtonGroup
+export default React.memo(CategoryButtonGroup)
