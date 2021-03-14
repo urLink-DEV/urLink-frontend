c2f4dba14ad3a1d92d6121da0778143f59db856b
yjkwon07 <dudwns9073@nate.com>
2021-03-14 16:24:27 +0900

    🐛 링크 저장시 stirng 길이만큼 저장되는 이슈 해결

--- src/main/pages/Home/LinkDropZone/index.jsx
+++ src/main/pages/Home/LinkDropZone/index.jsx
@@ -4,8 +4,9 @@ import { AddToPhotos as AddToPhotosIcon } from '@material-ui/icons'
 import clsx from 'clsx'
 import { useDispatch, useSelector } from 'react-redux'
 
-import { categorySelector, categoriesReadThunk } from '@modules/category'
+import { categoriesRead, categorySelector } from '@modules/category'
 import { linkCreateThunk, linksRead } from '@modules/link'
+import { useToast } from '@modules/ui'
 import { DROP_ZONE, DRAG, useDrag, useDropZone } from '@modules/ui'
 
 import useStyles from './style'
@@ -16,35 +17,30 @@ const { LINK_DROP_ZONE } = DROP_ZONE
 function LinkDropZone() {
   const classes = useStyles()
   const dispatch = useDispatch()
+  const { openToast } = useToast()
   const selectedCategory = useSelector(categorySelector.selectedCategory)
   const { listData } = useDrag(LINK)
   const { open } = useDropZone(LINK_DROP_ZONE)
 
-  const dispatchLinkCreate = useCallback(async () => {
-    const promises = []
-    listData.forEach((link) =>
-      promises.push(
-        dispatch(
-          linkCreateThunk({
-            categoryId: selectedCategory.id,
-            path: link.path,
-          })
-        )
-      )
-    )
-    return Promise.all(promises)
-  }, [dispatch, selectedCategory, listData])
+  const handleDropOnCardArea = useCallback(
+    async (e) => {
+      try {
+        e.stopPropagation()
+        const path = listData.reduce((prev, data) => prev.concat(data.path), [])
+        await dispatch(linkCreateThunk({ categoryId: selectedCategory.id, path }))
+        dispatch(linksRead.request({ categoryId: selectedCategory.id }))
+        dispatch(categoriesRead.request())
+        openToast({ type: 'success', message: '링크가 저장 되었습니다.' })
+      } catch (error) {
+        openToast({ type: 'error', message: error?.response?.data?.message || '네트워크 오류!!' })
+      }
+    },
+    [dispatch, listData, openToast, selectedCategory.id]
+  )
 
-  const handleDropOnCardArea = async (e) => {
-    e.stopPropagation()
-    await dispatchLinkCreate()
-    dispatch(categoriesReadThunk())
-    dispatch(linksRead.request({ categoryId: selectedCategory.id }))
-  }
-
-  const handleDragOverOnCardArea = (e) => {
+  const handleDragOverOnCardArea = useCallback((e) => {
     e.preventDefault()
-  }
+  }, [])
 
   return (
     <div
