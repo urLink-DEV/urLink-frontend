31331321679fca84de9dee9e44874fed80c9bbd6
yjkwon07 <dudwns9073@nate.com>
2021-03-19 23:13:36 +0900

    ⚡ background  chrome notifications => Notification - chrome의 addlistener의 다중 알림이 전역으로 관리하여 개별 메시지를 다루기 힘듬으로 내장 lib  활용

--- src/background/index.js
+++ src/background/index.js
@@ -1,7 +1,6 @@
 import { requestAlarmReadNotice } from '@/modules/alarmNotice'
 import { createTab } from '@/utils/chromeApis/tab'
 import { getAccessToken } from '@/utils/http/auth'
-import { notifications, notificationsAddListener } from '@utils/chromeApis/notifications'
 import { onMessage, REMOVE_TOKEN, UPDATE_TOKEN } from '@utils/chromeApis/onMessage'
 import { alarmSocket } from '@utils/http/ws'
 
@@ -12,16 +11,14 @@ function connectionWS() {
     alarmSocket.onConnection().setOnmessage((event) => {
       const { message, status } = JSON.parse(event.data)
       if (status === 'alarm') {
-        notifications('alarm', {
-          type: 'basic',
-          title: 'urLink 알람이 도착했습니다.',
-          message: message.url_title,
-          iconUrl: message.url_favicon_path,
+        const notification = new Notification('urLink 알람이 도착했습니다.', {
+          icon: message.url_favicon_path,
+          body: message.url_title,
         })
-        notificationsAddListener(() => {
+        notification.onclick = () => {
           requestAlarmReadNotice({ alarm_id: message.id })
           createTab(message.url_path)
-        })
+        }
       }
     })
   } catch (error) {
