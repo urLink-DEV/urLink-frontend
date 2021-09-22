ef92950eb597f9e96321f15c761808cbec1bae58
leesoo7595 <isoo7510@gmail.com>
2021-09-22 13:58:47 +0900

    :sparkles: ga 버튼 이벤트 트래킹 코드 추가

--- public/analytics.js
+++ public/analytics.js
@@ -19,3 +19,31 @@ ga('create', 'UA-207149982-2', 'auto')
 ga('set', 'checkProtocolTask', function () {})
 ga('send', 'pageview')
 console.log(ga.q)
+
+var _gaq = _gaq || []
+_gaq.push(['_setAccount', 'UA-207149982-2'])
+_gaq.push(['_trackPageview'])
+/**
+ * Track a click on a button using the asynchronous tracking API.
+ *
+ * See http://code.google.com/apis/analytics/docs/tracking/asyncTracking.html
+ * for information on how to use the asynchronous tracking API.
+ */
+function trackButtonClick(e) {
+  _gaq.push(['_trackEvent', e.target.id, 'clicked'])
+  console.log('track!')
+}
+
+/**
+ * Now set up your event handlers for the popup's `button` elements once the
+ * popup's DOM has loaded.
+ *
+ * TODO: id 있는 버튼 트래킹되는지 확인 후 button id 추가 필요
+ */
+document.addEventListener('DOMContentLoaded', function () {
+  var buttons = document.querySelectorAll('button')
+  console.log(buttons)
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', trackButtonClick)
+  }
+})
