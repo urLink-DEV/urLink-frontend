990eb582442d6c07932e814ce50f875ef7c1a58e
anxiubin <tnqls0120@naver.com>
2021-10-10 22:40:53 +0900

    🐛 ga 코드 수정 및 react-ga 세팅

--- public/analytics.js
+++ public/analytics.js
@@ -1,13 +1,20 @@
-var _AnalyticsCode = 'UA-207149982-2'
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-expressions */
 
-var _gaq = _gaq || []
-_gaq.push(['_setAccount', _AnalyticsCode])
-_gaq.push(['_trackPageview'])
-;(function () {
-  var ga = document.createElement('script')
-  ga.type = 'text/javascript'
-  ga.async = true
-  ga.src = 'https://ssl.google-analytics.com/ga.js'
-  var s = document.getElementsByTagName('script')[0]
-  s.parentNode.insertBefore(ga, s)
-})()
+;(function (i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r
+  ;(i[r] =
+    i[r] ||
+    function () {
+      ;(i[r].q = i[r].q || []).push(arguments)
+    }),
+    (i[r].l = 1 * new Date())
+  ;(a = s.createElement(o)), (m = s.getElementsByTagName(o)[0])
+  a.async = 1
+  a.src = g
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga')
+
+ga('create', 'UA-207149982-2', 'auto')
+ga('set', 'checkProtocolTask', null)
+ga('send', 'pageview')
